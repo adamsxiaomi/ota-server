de4d7dadb3ac2fd0225629a92157a9faae6c8c2d
adamsxiaomi <495231753@qq.com>
2019-12-30 20:33:46 +0800

    升级测试

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,59 @@ var router = express.Router();
 var multer = require('multer')
 var fs = require('fs');
 
-// var bodyParser = require('body-parser');
+
+// MQTT
+
+var mosca = require('mosca');
+var mqtt = require('mqtt')
+
+//mqtt server
+var ascoltatore = {
+    //using ascoltatore
+    //type: 'mongo',
+    //url: 'mongodb://localhost:27017/mqtt',
+    //pubsubCollection: 'ascoltatori',
+    //mongo: {}
+};
+
+var settings = {
+    port: 18886,
+    backend: ascoltatore
+};
+
+//client
+var client = mqtt.connect('tcp://172.16.0.224:18886')
+
+var fs = require('fs');
+var path = require('path');
+
+function displayFile(param) {
+    //转换为绝对路径
+    var param = path.resolve(param);
+    fs.stat(param, function (err, stats) {
+        //如果是目录的话，遍历目录下的文件信息
+        if (stats.isDirectory()) {
+            fs.readdir(param, function (err, file) {
+                file.forEach((e) => {
+                    //遍历之后递归调用查看文件函数
+                    //遍历目录得到的文件名称是不含路径的，需要将前面的绝对路径拼接
+                    var absolutePath = path.resolve(path.join(param, e));
+                    displayFile(absolutePath)
+                })
+            })
+        } else {
+            //如果不是目录，打印文件信息
+            console.log(param)
+        }
+    })
+}
+
+router.post('/update', function (req, res, next) {
+    console.log("updateing");
+    client.publish('/ROUTING/3/0100124B0013A4BE48/ota/sub', '升级可以开始了！');
+    return res.status(200).send("操作成功");
+    // return res.end();
+});
 
 
 /* GET login page. */
@@ -12,8 +64,11 @@ router.get('/home', function (req, res, next) {
 });
 
 router.get('/firewareManage', function (req, res, next) {
-    res.render('firewareManage', { title: 'firewareManage' });
+    displayFile('../')
+    res.end();
+    // res.render('firewareManage', { title: 'firewareManage' });
 });
+
 //封装函数
 function showMessage(message,res){
     var result=`<script>alert('${message}');history.back()</script>`;
